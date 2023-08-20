8ecd3f57a3fcefa33c3a6a816439105ce50a683d
Ibrahim Olawuyi <ibrohimolawuyi4peace@gmail.com>
2023-08-20 14:30:10 +0100

    eleventh commit

--- js/script.js
+++ js/script.js
@@ -3,6 +3,8 @@ const selectTag = document.querySelectorAll("select");
 selectTag.forEach(tag =>{
     for (const country_code in countries){
         let selected;
-        if(id == 0 && country_code == "en-GB"){}
+        if(id == 0 && country_code == "en-GB"){
+            selected = "selected";
+        }
     }
 });
