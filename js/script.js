1f0d2488420bf0b7fa05cc3de2a4519af16de17f
Ibrahim Olawuyi <ibrohimolawuyi4peace@gmail.com>
2023-08-20 14:29:26 +0100

    tnth commit

--- js/script.js
+++ js/script.js
@@ -3,5 +3,6 @@ const selectTag = document.querySelectorAll("select");
 selectTag.forEach(tag =>{
     for (const country_code in countries){
         let selected;
+        if(id == 0 && country_code == "en-GB"){}
     }
 });
