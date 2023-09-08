b2e24945499b19610410cb87d27f09fe47607018
Ibrahim Olawuyi <ibrohimolawuyi4peace@gmail.com>
2023-09-08 11:50:37 +0100

    22

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-selectTag = document.querySelectorAll("select");
-exchangeIcon = document.querySelector(".exchange");
-const forText = document.querySelector(".form-text")
-toText = document.querySelector(".to-text")
-translateBtn = document.querySelector("button")
+const fromText = document.querySelector(".form-text"),
+selectTag = document.querySelectorAll("select"),
+exchangeIcon = document.querySelector(".exchange"),
+toText = document.querySelector(".to-text"),
+translateBtn = document.querySelector("button"),
+icons = document.querySelectorAll(".row i")
 id = 0
 selectTag.forEach(tag =>{
     for (const country_code in countries){
@@ -19,7 +20,7 @@ selectTag.forEach(tag =>{
 
 
 exchangeIcon.addEventListener("click" , () =>{
-    let tempText = forText.value,
+    let tempText = fromText.value,
     tempLang = selectTag[0].value;
     forText.value = toText.value;
     selectTag[1] = selectTag[0].value;
@@ -29,13 +30,30 @@ exchangeIcon.addEventListener("click" , () =>{
 });
 
 translateBtn.addEventListener("click" , () =>{
-    let text = forText.value,
+    let text = fromText.value,
     translateFrom = selectTag[0].value,
     translateTo = selectTag[1].value;
     let apiUrl = `https://api.mymemory.translated.net/get?q=${text}&langpair=${translateFrom}|${translateTo}`;
     fetch(apiUrl).then(res => res.json()).then(data => {
-        console.log(data);
         toText.value = data.responseData.translatedText;
 
+    });
+});
+icons.forEach(icons =>{
+    icons.addEventListener("click", ({target}) =>{
+        if (target.classList.contains("bi-clipboard-check")) {
+            if(target.id == "from"){
+                navigate.clipboard.writeText(fromText.value)
+            }else{
+                navigate.clipboard.writeText(toText.value)
+            }
+        }
+        else{
+            let utternance;
+                if(target.id == "from"){
+                    utternance = new SpeechSynthesisUtterance(fromText.value);
+                }else{
+                    utternance = new SpeechSynthesisUtterance(toText.value);
+                }        }
     });
 });
